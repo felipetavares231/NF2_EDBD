65d4417b38b2853e6ab8fb6583d6167ce907c5cf
luisvcsilva <luisviniciuscostasilva@gmail.com>
2026-08-20 18:18:13 -0300

    codigo de bst + avl + apps demo

--- REPO/codigos/lista/lista_ligada.c
+++ REPO/codigos/lista/lista_ligada.c
@@ -1,36 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lista_ligada.h"
 
+void inserir_inicio(No **lista, int valor) {
+    No *novo = malloc(sizeof(No));
+    novo->dado = valor;
+    novo->prox = *lista;
+    *lista = novo;
+}
 
-typedef struct No{
-
-    int dado;
-
-    struct No *prox;
-
-
-}No;
-
-
-
-int main(){
-
-    No *novo;
-
-
-    novo = malloc(sizeof(No));
-
-
-    novo->dado = 10;
-
+void inserir_fim(No **lista, int valor) {
+    No *novo = malloc(sizeof(No));
+    novo->dado = valor;
     novo->prox = NULL;
 
+    if (*lista == NULL) {
+        *lista = novo;
+    } else {
+        No *aux = *lista;
+        while (aux->prox != NULL) {
+            aux = aux->prox;
+        }
+        aux->prox = novo;
+    }
+}
 
-    printf("%d",novo->dado);
+No* buscar(No *lista, int valor) {
+    No *aux = lista;
+    while (aux != NULL) {
+        if (aux->dado == valor) {
+            return aux;
+        }
+        aux = aux->prox;
+    }
+    return NULL;
+}
 
+void remover_inicio(No **lista) {
+    if (*lista != NULL) {
+        No *aux = *lista;
+        *lista = (*lista)->prox;
+        free(aux);
+    }
+}
 
-    free(novo);
+void imprimir(No *lista) {
+    No *aux = lista;
+    while (aux != NULL) {
+        printf("%d -> ", aux->dado);
+        aux = aux->prox;
+    }
+    printf("NULL\n");
+}
 
-
-    return 0;
+void liberar_lista(No **lista) {
+    while (*lista != NULL) {
+        remover_inicio(lista);
+    }
 }
